9488039b7a4f4533fc632a0fd62702c25b1f9ece
Domenico Montalto <montaltodomenico92@gmail.com>
2023-10-02 19:14:46 -0600

    Initial Commit

---
+++ LetsCode/LetsCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsCodeApp.swift
+//  LetsCode
+//
+//  Created by Domenico Montalto on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LetsCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
