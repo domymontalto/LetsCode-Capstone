ecd442c06037b5c7f766579ea0da105d3bd2ebd0
Domenico Montalto <montaltodomenico92@gmail.com>
2023-10-05 19:12:45 -0600

    Modified the time/duration of contents and tests in data.json Modified HomeView to display the Learning Card of each Module by adding ScrollView, LazyVStack, ForEach and all the various components of the Learning Card

--- LetsCode/Views/Home/HomeView.swift
+++ LetsCode/Views/Home/HomeView.swift
@@ -14,11 +14,90 @@ struct HomeView: View {
     
     var body: some View {
 
-            Text("Hello, world!!!")
+        ScrollView {
+            
+            LazyVStack {
+                
+                ForEach(model.modules) { module in
+                    
+                    //Learning Card
+                    
+                    ZStack {
+                        
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+                        
+                        HStack {
+                            
+                            //Image
+                            Image(module.category)
+                                .resizable()
+                                .frame(width: 116, height: 116)
+                                .clipShape(Circle())
+                            
+                            Spacer()
+                            
+                            //Text
+                            VStack(alignment: .leading, spacing: 10) {
+                                
+                                //HeadLine
+                                Text("Learn \(module.category)")
+                                    .fontWeight(.bold)
+                                
+                                //Description
+                                Text(module.content.description)
+                                    .font(.caption)
+                                    .padding(.bottom, 20)
+                                
+                                
+                                //Icons
+                                HStack {
+                                    
+                                    //Number of lessons/questions
+                                    Image(systemName: "text.book.closed")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                    
+                                    Text("\(module.content.totLessons) Lessons")
+                                        .font(Font.system(size: 10))
+                                    
+                                    Spacer()
+                                    
+                                    //Time
+                                    
+                                    Image(systemName: "clock")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                    
+                                    
+                                    
+                                    Text("\(module.content.time)")
+                                        .font(Font.system(size: 10))
+                                    
+                                }
+                                
+                            }
+                            .padding(.leading)
+                            
+                            
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                    
+                    
+                    //Test Card
+                    
+                }
+            }
             .padding()
+        }
     }
 }
 
 #Preview {
     HomeView()
+        .environmentObject(ContentModel())
 }
